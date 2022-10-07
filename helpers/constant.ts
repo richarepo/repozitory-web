41e74b6217ddb0126336895855790da67ddee44f
richarepo <richa@repozitory.com>
2022-10-07 18:07:57 +0530

    Fixed Css of cards in percentage

--- helpers/constant.ts
+++ helpers/constant.ts
@@ -9,19 +9,19 @@ import { ChatIcon, CheckCircleIcon, SunIcon, TimeIcon, ViewIcon } from "@chakra-
 
 export const WHY_ORGANISATION_OPTIONS = [
   {
-    logo: SizeyLogo,
+    logo: GetStatusLogo,
     logoWidth: 110,
     bgColor: '#b1e2bf',
-    title: "Software that digitize the eye test within five minutes",
-    description: "…a digital platform which could be an alternative to the traditional lab coat and lens approach for diagnosis…",
+    title: "Leading technology for project management in urban renewal.",
+    description: "An urban renewal project is a complex and long project involving many professionals. To help you succeed in the long and complicated process, the Get Status technological system was developed. The system contributes efficiency and satisfaction of the apartment owners",
     benefits: [
       {
-        label: "Reduced eye test",
-        description: "cost by 75%"
+        label: "Customer Accessbility",
+        description: "Secure access to the project status - through the computer, mobile phone and app."
       },
       {
-        label: "Complete eye test",
-        description: "using smartphone within 10-15 minutes"
+        label: "Easy Management",
+        description: "You can assign tasks to the relevant parties, reminders, updates application and more"
       }
     ]
   },
@@ -29,16 +29,16 @@ export const WHY_ORGANISATION_OPTIONS = [
     logo: EargoLogo,
     logoWidth: 130,
     bgColor: '#b4c2e9',
-    title: "EV charging platform to revolutionize the way we power everything",
-    description: "FreeWire‘s Mobi Gen product line offers clean and quiet on-site power to substitute…",
+    title: "HEARING MADE SIMPLE.",
+    description: "Eargo is a medical device company on a mission to improve hearing health. Our innovative product and go-to-market approach address the major challenges of traditional hearing aid adoption, including social stigma, accessibility and cost.",
     benefits: [
       {
-        label: "$125 Million",
-        description: "Funding for Next Gen development"
+        label: "Remote Support",
+        description: "No Appoinments And Lifetime, remote support in telecare hearing health."
       },
       {
-        label: "Used by big companies",
-        description: "Walmart, Uber, Volvo, Linkedin, BellEnergy"
+        label: "Quality Product",
+        description: "You can hear crisp and clear enter your in ear naturally."
       }
     ]
   },
@@ -46,33 +46,33 @@ export const WHY_ORGANISATION_OPTIONS = [
     logo: FidelixLogo,
     logoWidth: 110,
     bgColor: '#ecccb7',
-    title: "Improving the online web experience for International Hockey Federation (FIH)",
-    description: "…they wanted increased user engagement, usability, discoverability, while providing…",
+    title: "Building Automation, Management and Security systems.",
+    description: "The aim is to produce buildings with user-oriented digitalization and optimum conditions all the time, without any waste of energy and other resources. And always cost-effectively, too. Improves the customer experience and productivity and also makes life easier for property owners",
     benefits: [
       {
-        label: "Completed website",
-        description: "within 3 months before World Cup"
+        label: "Reporting",
+        description: "Up-to-date information to support decision-making. Clear reports on current conditions"
       },
       {
-        label: "Scalable website",
-        description: "to handle millions of visitors"
+        label: "User Oriented",
+        description: "Fidelix solutions provides manageable dashboard and helps the user to conserve energy and resources."
       }
     ]
   },
   {
-    logo: GetStatusLogo,
+    logo: SizeyLogo,
     logoWidth: 110,
     bgColor: '#eadab8',
-    title: "M-payment solution to modernise retail buying experience",
-    description: "…over 70% of consumers say that if they get benefits of loyalty points or perks in retail stores, they are ready…",
+    title: "The world's most accurate size recommendation tool for fashion e-ecommerce.",
+    description: "MySizey creates a seamless shopping experience by offering automated size recommendations and a virtual fitting room that allows shoppers to try on clothes in the comfort of their own homes.MySizey is a web-based plugin that integrates with fashion ecommerce sites to generate automatic sizing recommendations for shoppers based on their profile.",
     benefits: [
       {
-        label: "10,000+ users",
-        description: "60,000+ retail locations in USA"
+        label: "Perfect Size",
+        description: "No more return on e-commerce with accurate measurements on your products"
       },
       {
-        label: "Cryptocurrency",
-        description: "was made spendable in retail store"
+        label: "Virtual experience",
+        description: "Creating a 3D Virtual Fitting Room that shoppers can use in the comfort of their homes."
       }
     ]
   }
@@ -81,48 +81,48 @@ export const WHY_ORGANISATION_OPTIONS = [
 export const ALL_REPOZITORY_WAYS: any = [
   {
     label: 'Providing easy solutions',
-    bottom: 50,
-    left: 320,
+    bottom: '10%',
+    left: '30%',
     icon: TimeIcon,
     iconBg: '#e8f3fe',
     iconColor: '#569aeb'
   },
   {
     label: 'Better code quality',
-    top: 50,
-    left: 320,
+    top: '5%',
+    left: '30%',
     icon: SunIcon,
     iconBg: '#fceeef',
     iconColor: '#e6717b'
   },
   {
     label: 'Better time management',
-    top: 200,
-    left: -100,
+    top: '30%',
+    left: '-14%',
     icon: TimeIcon,
     iconBg: '#e8f3fe',
     iconColor: '#569aeb'
   },
   {
     label: 'Using always updated technology',
-    right: 100,
-    bottom: -100,
+    right: '8%',
+    bottom: '-20%',
     icon: ViewIcon,
     iconBg: '#eef9f2',
     iconColor: '#96e1c2'
   },
   {
     label: 'Waterfall model',
-    right: 100,
-    top: -100,
+    right: '8%',
+    top: '-20%',
     icon: CheckCircleIcon,
     iconBg: '#ebf4fe',
     iconColor: '#5f8bce'
   },
   {
     label: 'Providing better user experience',
-    right: 100,
-    bottom: 170,
+    right: '8%',
+    bottom: '30%',
     icon: ChatIcon,
     iconBg: '#eef9f2',
     iconColor: '#96e1c2'
